e99492046221dff6332047436dfb264485a40430
tereom <teresa.orti.mancera@gmail.com>
2018-03-03 10:53:44 -0600

    Función que calcula las estimaciones usando estimador combinado, separado y porcentaje de polígonos

--- R/calcular_est.R
+++ R/calcular_est.R
@@ -20,7 +20,7 @@
 
 #' @importFrom magrittr %>%
 #' @export
-calcular_ests <- function(estados, edo_val, bits_df){
+calcular_ests <- function(estados, edo_val, bits_df, marco_muestral){
     # variables:
     # predicted - MADMEX
     # Interpr1_p, Interpr2_p (interpretaciones Pedro)
@@ -35,143 +35,120 @@ calcular_ests <- function(estados, edo_val, bits_df){
             M_h = sum(area),
             N_h = n()
         )
-    # rm(marco_muestral)
-
-    # preparamos los datos, agregando el estrato, oid es necesario para unir los
-    # datos y no está incluída en el marco muestral (valdria la pena cambiar eso)
-    # load("datos_procesados/2017-08-18_pais_df.Rdata")
-    edo_df <- dplyr::filter(pais_df, edo == estado)
-    # rm(pais_df)
-
     edo_val_df <- edo_val %>%
-        dplyr::left_join(edo_df, by = c("oid", "tile")) %>%
-        dplyr::select(oid, tile, predicted = predicted.y, edo = edo.x, area_r,
-            Interpr1_p, Interpr2_p) %>%
-        dplyr::mutate(area_cat = case_when(
-            .$area_r <= 50000 ~ "[5000,50000]",
-            .$area_r <= 100000 ~ "(50000,100000]",
-            .$area_r <= 500000 ~ "(100000,500000]",
-            TRUE ~ "(500000,1200000]"),
-            estrato = paste(predicted, edo, area_cat, sep = "-"),
-            y_h = (predicted == Interpr1_p | predicted == Interpr2_p)
-        ) %>%
-        dplyr::left_join(estratos_area)
-
-
-
-    ## % área clasificado correcto por Madmex (predicted)
-    p_hs <- edo_val_df %>%
-        dplyr::group_by(estrato) %>%
-        dplyr::do(ratio_est(M = .$area_r, y = .$y_h, N = .$N_h[1])) %>%
-        dplyr::left_join(estratos_area, by = c("estrato"))
-
-    est_madmex_area <- strat_est(p_h = p_hs$p_hat, n_h = p_hs$n, M_h = p_hs$M_h,
-        N_h = p_hs$N_h, s2_h = p_hs$s2_p_hat)
-
-    edo_bits_df <- bits_df %>%
-        dplyr::filter(edo %in% edos)
-
-    p_hs <- edo_bits_df %>%
-        dplyr::mutate(y_h = (interpreta == Interpr1_p | interpreta == Interpr2_p)) %>%
-        dplyr::left_join(estratos_area) %>%
-        dplyr::group_by(estrato) %>%
-        dplyr::do(ratio_est(M = .$area_r, y = .$y_h, N = .$N_h[1])) %>%
-        dplyr::left_join(estratos_area, by = c("estrato"))
-
-    est_bits_area <- strat_est(p_h = p_hs$p_hat, n_h = p_hs$n, M_h = p_hs$M_h,
-        N_h = p_hs$N_h, s2_h = p_hs$s2_p_hat)
-
-    ## % polígonos clasificados correcto Bits
-    p_porcent <- edo_bits_df %>%
-        dplyr::mutate(y_h = (interpreta == Interpr1_p | interpreta == Interpr2_p))%>%
-        dplyr::left_join(estratos_area) %>%
-        dplyr::group_by(estrato) %>%
-        dplyr::summarise(
-            n_h = n(),
-            N_h = first(N_h),
-            prop_h = N_h / sum(estratos_area$N_h),
-            f_h = n_h / first(N_h),
-            p_h = mean(y_h),
-            p_aux_h = prop_h * p_h,
-            var_aux = (1 - f_h) * prop_h ^ 2 * p_h * (1 - p_h) / (n_h - 1)
-        )
-
-    ests_bits_porcent <- c(p_hat = sum(p_porcent$p_aux_h),
-        se_p_hat = sqrt(sum(p_porcent$var_aux[p_porcent$N_h > 1])))
-
-    # Madmex
-    p_porcent <- edo_bits_df %>%
-        dplyr::mutate(y_h = (predicted.x == Interpr1_p | predicted.x == Interpr2_p)) %>%
-        dplyr::left_join(estratos_area) %>%
-        dplyr::group_by(estrato) %>%
-        dplyr::summarise(
-            n_h = n(),
-            N_h = first(N_h),
-            prop_h = N_h / sum(estratos_area$N_h),
-            f_h = n_h / first(N_h),
-            p_h = mean(y_h),
-            p_aux_h = prop_h * p_h,
-            var_aux = (1 - f_h) * prop_h ^ 2 * p_h * (1 - p_h) / (n_h - 1)
-        )
-
-    ests_madmex_porcent <- c(p_hat = sum(p_porcent$p_aux_h),
-        se_p_hat = sqrt(sum(p_porcent$var_aux[p_porcent$N_h > 1])))
-
-    edo_svy <-  edo_bits_df %>%
+        dplyr::left_join(marco_muestral, by = c("edo", "oid", "tile")) %>%
+        dplyr::left_join(select(bits_df, -predicted), by = c("edo", "oid", "tile")) %>%
+        # dplyr::left_join(estratos_area, by = c("estrato")) %>%
         dplyr::mutate(
             id = stringr::str_c(oid, tile, sep = "-"),
-            class_madmex = factor(predicted.x, levels = 1:32),
+            class_madmex = factor(predicted, levels = 1:32),
             class_bits = factor(interpreta, levels = 1:32),
             class_expert_1 = factor(Interpr1_p, levels = 1:32),
             class_expert_2 = factor(Interpr2_p, levels = 1:32),
-            y_madmex = (predicted.x == Interpr1_p | predicted.x == Interpr2_p),
-            y_M_madmex = y_madmex * area_r,
+            y_madmex = (predicted == Interpr1_p | predicted == Interpr2_p),
             y_bits = (interpreta == Interpr1_p | interpreta == Interpr2_p),
-            y_M_bits = y_bits * area_r
+            y_M_madmex = y_madmex * area,
+            y_M_bits = y_bits * area
         ) %>%
-        dplyr::select(id, edo, class_madmex, class_expert_1, class_expert_2,
-            class_bits, N_h, y_madmex,
-            y_M_madmex, y_bits, y_M_bits, estrato, area_r
-        )
+        dplyr::select(id, edo, estrato, area, class_madmex:y_M_bits)
+
+    ests_madmex_area_sep <- separate_ratio_est(data = edo_val_df,
+        estratos_area = estratos_area, stratum = estrato, y = y_madmex, M = area)
+
+    ests_bits_area_sep <- separate_ratio_est(data = edo_val_df,
+        estratos_area = estratos_area, stratum = estrato, y = y_bits, M = area)
+
+    # usando srvyr para calcular el combinado
+    edo_svy <- edo_val_df %>%
+        dplyr::left_join(estratos_area, by = "estrato")
 
     edo_design <- edo_svy %>%
         srvyr::as_survey_design(ids = id, strata = estrato, fpc = N_h)
 
-    est_area_comb <- edo_design %>%
+    ests_area_combined <- edo_design %>%
         srvyr::summarise(
             est_area_madmex = srvyr::survey_ratio(numerator = y_M_madmex,
-                denominator = area_r),
+                denominator = area),
             est_area_bits = srvyr::survey_ratio(numerator = y_M_bits,
-                denominator = area_r)
+                denominator = area)
         )
 
-    est_area_clase_madmex <- edo_design %>%
-        srvyr::group_by(class_madmex) %>%
-        srvyr::summarise(
-            est_area_madmex = srvyr::survey_ratio(numerator = y_M_madmex,
-                denominator = area_r),
-        )
-
-    est_area_clase_bits <- edo_design %>%
-        srvyr::group_by(class_bits) %>%
-        srvyr::summarise(
-            est_area_bits = srvyr::survey_ratio(numerator = y_M_bits,
-                denominator = area_r)
-        )
-
-    est_area_edo <-  edo_design %>%
+    ests_area_edo_combined <- edo_design %>%
         srvyr::group_by(edo) %>%
         srvyr::summarise(
             est_area_madmex = srvyr::survey_ratio(numerator = y_M_madmex,
-                denominator = area_r),
+                denominator = area),
             est_area_bits = srvyr::survey_ratio(numerator = y_M_bits,
-                denominator = area_r)
+                denominator = area)
         )
 
-    return(list(est_madmex_area_sep = est_madmex_area,
-        est_bits_area_sep = est_bits_area,
-        ests_madmex_porcent = ests_madmex_porcent,
-        ests_bits_porcent = ests_bits_porcent,
-        est_area_comb, est_area_clase_madmex,
-        esta_area_clase_bits, est_area_edo))
+    ests_area_clase_madmex_combined <- edo_design %>%
+        group_by(class_madmex) %>%
+        summarise(
+            est_area_madmex = srvyr::survey_ratio(numerator = y_M_madmex,
+                denominator = area)
+        )
+
+    ests_area_clase_bits_combined <- edo_design %>%
+        group_by(class_bits) %>%
+        summarise(
+            est_area_madmex = srvyr::survey_ratio(numerator = y_M_bits,
+                denominator = area),
+        )
+
+    ests_area_clase_experto_combined <- edo_design %>%
+        group_by(class_expert_1) %>%
+        summarise(
+            est_area_madmex = survey_ratio(numerator = y_M_madmex, denominator = area_r),
+            est_area_bits = survey_ratio(numerator = y_M_bits, denominator = area_r)
+        )
+
+    ests_porcent_combined <- edo_design %>%
+        srvyr::summarise(
+            est_porcent_madmex = srvyr::survey_mean(numerator = y_madmex),
+            est_porcent_bits = srvyr::survey_mean(numerator = y_bits)
+        )
+
+    ests_porcent_edo_combined <- edo_design %>%
+        srvyr::group_by(edo) %>%
+        srvyr::summarise(
+            est_porcent_madmex = srvyr::survey_mean(numerator = y_madmex),
+            est_porcent_bits = srvyr::survey_mean(numerator = y_bits)
+        )
+
+    ests_porcent_clase_madmex_combined <- edo_design %>%
+        group_by(class_madmex) %>%
+        summarise(
+            est_porcent_madmex = srvyr::survey_mean(numerator = y_madmex)
+        )
+
+    ests_porcent_clase_bits_combined <- edo_design %>%
+        group_by(class_bits) %>%
+        summarise(
+            est_porcent_madmex = srvyr::survey_mean(numerator = y_bits)
+        )
+
+
+    ests_porcent_clase_experto_combined <- edo_design %>%
+        group_by(class_expert_1) %>%
+        summarise(
+            est_area_madmex = survey_mean(numerator = y_madmex),
+            est_area_bits = survey_mean(numerator = y_bits)
+        )
+
+
+
+    return(list(est_madmex_area_sep = ests_madmex_area_sep,
+        ests_bits_area_sep = ests_bits_area_sep,
+        ests_area_combined = ests_area_combined,
+        ests_area_edo_combined = ests_area_edo_combined,
+        ests_area_clase_madmex_combined = ests_area_clase_madmex_combined,
+        ests_area_clase_bits_combined = ests_area_clase_bits_combined,
+        ests_area_clase_experto_combined = ests_area_clase_experto_combined,
+        ests_porcent_combined = ests_porcent_combined,
+        ests_porcent_edo_combined = ests_porcent_edo_combined,
+        ests_porcent_clase_madmex_combined = ests_porcent_clase_madmex_combined,
+        ests_porcent_clase_bits_combined = ests_porcent_clase_bits_combined,
+        ests_porcent_clase_experto_combined = ests_porcent_clase_experto_combined
+        ))
 }
